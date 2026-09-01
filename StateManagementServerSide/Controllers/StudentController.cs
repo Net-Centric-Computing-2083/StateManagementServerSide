5ef6bbd69facfcdf8e6c800e50c115a48ba0b726
Nabin Kandel <nkandel208@gmail.com>
2026-08-31 23:15:13 -0700

    Add project files.

---
+++ StateManagementServerSide/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StateManagementServerSide.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Save(string name)
+        {
+            //SetSession
+            HttpContext.Session.SetString("StudentName", name);
+
+            //TempData
+            TempData["Message"] = "Student saved successfully!";
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult Details()
+        {
+            //GetSession
+            var studentName = HttpContext.Session.GetString("StudentName");
+
+            //GetTempData
+            string message = TempData["Message"]?.ToString() 
+                ?? "No Message";
+
+            //GetHttpContext
+            string context = HttpContext.Request.Method;
+
+            ViewBag.StudentName = studentName;
+            ViewBag.Message = message;
+            ViewBag.Context = context;
+            return View();
+        }
+    }
+}
---
+++ StateManagementServerSide/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StateManagementServerSide.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
