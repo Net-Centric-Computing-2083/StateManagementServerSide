3368199e6d34ab8cc9250c6988f0723d79db815e
Nabin Kandel <nkandel208@gmail.com>
2026-09-03 10:01:32 -0700

    Cookie implementation

--- StateManagementServerSide/Controllers/HomeController.cs
+++ StateManagementServerSide/Controllers/HomeController.cs
@@ -21,5 +21,27 @@ namespace StateManagementServerSide.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        //SetCookie
+        public IActionResult SetCookie(string name, int age, string Address)
+        {
+            CookieOptions options = new CookieOptions();
+            options.Expires = DateTime.Now.AddDays(1);
+            Response.Cookies.Append("UserName", name, options);
+            Response.Cookies.Append("Age", age.ToString(), options);
+            Response.Cookies.Append("Address", Address, options);
+            return RedirectToAction("GetCookie");
+        }
+        //GetCookie
+        public IActionResult GetCookie()
+        {
+            string name = Request.Cookies["UserName"];
+            string age = Request.Cookies["Age"];
+            string address = Request.Cookies["Address"];
+            ViewBag.Name = name;
+            ViewBag.Age = age;
+            ViewBag.Address = address;
+            return View();
+        }
     }
 }
